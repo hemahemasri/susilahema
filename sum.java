c7cea078c120810540501397dec8524752a460c1
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 20:58:03 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class sum 
+{
+public static void main(String[]args)
+{
+Scanner sd=new Scanner(System.in)
+System.out.println("enter the values");
+c=sd.nextInt
+for(int i==0;i<=n;i++)
+{
+ int a,b,c;;
+ c=a+b;
+ System.out.println(c);
+ }
+ }
+ }
+ 
