35a8b54874f658fe0b67eec5c9c734505061e2c6
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 16:44:49 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+publilc class aiphabet 
+{
+public static void main(String[]args)
+{
+Scanner sg=new Scanner(System.in);
+System.ou.println("enter the alphabet");
+ch=sg.next();charAt(0);
+if((ch>=a && ch<=z)||(ch>=A && ch<=Z)
+{
+System.out.println("it is an alphabet");
+else
+{
+System.out.println("it is not an alphabet);
+}
+}
+System.out.println(ch);
+}
+}
