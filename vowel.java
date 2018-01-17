c062c7c9b91960489cab3885b09ee7c0b686b7f8
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 16:31:21 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class vowel
+{
+public static void main(String[]args)
+{
+Scanner sf= new Scanner(System.in);
+System.out.println("enter the letters");
+b=sf.next();
+if(b==a||b==e||b==i||b==o||b==u)
+{
+System.out.println("the given letter is vowel");
+else
+{
+System.out.println("the given letter is nonvowel");
+}
+}
+System.out.println(b);
+}
+}
