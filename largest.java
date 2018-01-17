37e6af339a24ae6414b828b12f237c26fca2a5e6
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 16:55:40 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class largest
+{
+public static void main(string[]args)
+{
+Scanner sh=new Scanner(System.in)
+System.out.println("enter the testcase");
+k=sh.nextInt();
+k=100;
+h=sh.nextInt();
+h=90;
+s=sh.nextInt();
+s=50;
+if((k>=h)&(k>=s))
+{
+Systemout.println("k is larger");
+else
+{
+System.out.println("there is no larger");
+}
+}
+System.out.println(k);
+System.out.println(h);
+System.out.println(s);
+}
+}
