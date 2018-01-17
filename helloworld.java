ce80768d974fc643c621ad0395eea66558bd7d77
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 20:40:50 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class 
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in)
+System.out.println("enter the String");
+s=sc.next();
+int n =10;
+for(inti==0;i<=n;i++)
+{
+System.out.println("hello world");
+}
+System.out.priintln(s);
+}
+}
