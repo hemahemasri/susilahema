27ca10e6107a5ef19cc2e2438dc6e3f2e5694658
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 16:13:32 +0530

    Create hema.java

---
+++ hema.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class system 
+{
+public static void main(String[]args)
+{
+Scannersc=new Scanner(System.in);
+System.out.println("enter the test case");
+n=sc.nextInt();
+if(n/2==0)
+{
+Sytem.out.println("the given is even");
+else
+{
+Sytem.out.println("the given is odd");
+}
+}
+System.out.println(n);
+}
+}
