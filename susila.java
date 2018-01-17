0ef10dd74e4e6c2cf1439aedfc1b312d2ea1824c
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-17 16:23:08 +0530

    Create susila.java

---
+++ susila.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class test
+{
+public static void main(String[]args)
+{
+Scanner sd=new Scanner(System.in);
+System.out.priontln("enter the terms");
+n=sd.nextInt();
+if(n>0)
+{
+System.out.println("the given num is positive");
+else
+{
+System.out.println("the given num is negative");
+}
+}
+System.out.println(n);
+}
+}
